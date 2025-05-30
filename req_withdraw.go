03497ead56e95ca322fb7c3f392fa0d539c2d859
cyrildou <cyrildou@live.cn>
2025-05-31 02:40:23 +0800

    up

---
+++ req_withdraw.go
@@ -0,0 +1,21 @@
+package go_payasia
+
+import (
+	"github.com/asaka1234/go-payasia/utils"
+	"github.com/mitchellh/mapstructure"
+)
+
+// 并不需要发Hhtp请求出去,纯粹是一个计算签名
+func (cli *Client) Withdraw(req PayAsiaWithdrawReq) (map[string]interface{}, error) {
+	var paramMap map[string]interface{}
+	mapstructure.Decode(req, &paramMap)
+
+	//补充公共字段
+	paramMap["datafeed_url"] = cli.Params.WithdrawBackUrl //回调地址
+
+	signStr := utils.Sign(paramMap, cli.Params.AccessKey)
+	paramMap["sign"] = signStr
+	paramMap["url"] = cli.Params.WithdrawUrl //实际前端post from action的地址
+
+	return paramMap, nil
+}
